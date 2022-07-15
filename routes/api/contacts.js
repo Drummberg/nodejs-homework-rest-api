4e3a52e0fa4444e1bbf5c8af747766ed19932dd7
Drummberg <drummberg@gmail.com>
2022-07-16 00:58:57 +0300

    commits

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,81 @@
-const express = require('express')
+const express = require('express');
+const Joi = require('joi');
 
-const router = express.Router()
+const contacts = require("../../models/contacts");
+
+const {createError} = require("../../helpers");
+
+const router = express.Router();
+
+const contactAddData = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required()
+})
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const result = await contacts.listContacts();
+  res.json(result);
+  next();
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+   try {
+    const { contactId } = req.params;
+    const contact = await contacts.getContactById(contactId);
+
+      if(!contact) {
+            throw createError(404);
+        }
+        res.json(contact)
+    } catch (error) {
+        next(error);
+    }
+});
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const { error } = contactAddData.validate(req.body);
+    if (error) {
+      throw createError(400, error.message);
+    }
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+        next(error);
+    }
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+ try {
+        const { contactId } = req.params;
+        const result = await contacts.removeContact(contactId);
+        if(!result){
+            throw createError(404)
+        }
+        res.json({
+            message: "Book deleted"
+        })
+    } catch (error) {
+        next(error);
+    }
+});
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    try {
+        const {error} = contactAddData.validate(req.body);
+        if(error) {
+            throw createError(400, error.message);
+        }
+        const {contactId} = req.params;
+        const result = await contacts.updateContactById(contactId, req.body);
+        if(!result) {
+            throw createError(404);
+        }
+        res.json(result);
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = router
